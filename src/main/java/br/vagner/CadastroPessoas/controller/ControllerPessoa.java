4eb074bb69c781710a55a448b15fd87bc59de515
Vagner Souza <juninfps0@gmail.com>
2026-01-21 20:34:02 -0300

    anotação RestController adicionada (tinha esquecido)

--- src/main/java/br/vagner/CadastroPessoas/controller/ControllerPessoa.java
+++ src/main/java/br/vagner/CadastroPessoas/controller/ControllerPessoa.java
@@ -1,14 +1,16 @@
 package br.vagner.CadastroPessoas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class ControllerPessoa {
 
     @GetMapping("/teste")
     public String teste(){
         return "Teste API";
     }
-    
+
 }
