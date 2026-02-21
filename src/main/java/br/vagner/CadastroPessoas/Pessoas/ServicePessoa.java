c258844c61f1e47312827cf54fc4549fa72e83ab
Vagner Souza <juninfps0@gmail.com>
2026-02-21 09:59:10 -0300

    feat: injeta a dependencia do repository e metodo de listar pessoas feito

--- src/main/java/br/vagner/CadastroPessoas/Pessoas/ServicePessoa.java
+++ src/main/java/br/vagner/CadastroPessoas/Pessoas/ServicePessoa.java
@@ -1,4 +1,21 @@
 package br.vagner.CadastroPessoas.Pessoas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ServicePessoa {
+
+    private RepositoryPessoa repositoryPessoa;
+
+    public  ServicePessoa(RepositoryPessoa repositoryPessoa1){
+        this.repositoryPessoa = repositoryPessoa1;
+    }
+
+    //Listar pessoas
+    public List<Pessoa> ListarPessoas(){
+        return repositoryPessoa.findAll();
+    }
+
 }
