7a32b90cf6afb4127df2695e9aba734d243b6abb
Vagner Souza <juninfps0@gmail.com>
2026-02-21 09:55:30 -0300

    feat: chamando o REpositoryAtividade e metodo de listar atividades feito

--- src/main/java/br/vagner/CadastroPessoas/Atividades/ServiceAtividade.java
+++ src/main/java/br/vagner/CadastroPessoas/Atividades/ServiceAtividade.java
@@ -1,4 +1,19 @@
 package br.vagner.CadastroPessoas.Atividades;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ServiceAtividade {
+
+    RepositoryAtividade repositoryAtividades;
+
+    public ServiceAtividade(RepositoryAtividade repositoryAtividades) {
+        this.repositoryAtividades = repositoryAtividades;
+    }
+
+    public List<Atividade> ListarAtividades(){
+        return repositoryAtividades.findAll();
+    }
+
 }
