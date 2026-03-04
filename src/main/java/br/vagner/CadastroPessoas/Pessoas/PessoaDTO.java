7791caaf31d59989d1554f487ff1c44f8cd2c23e
Vagner Souza <juninfps0@gmail.com>
2026-03-04 20:44:53 -0300

    Chore: inclui a versão do DTO pela entidade e mapeia manualmente no pessoa Mapper os caminhos entidade x DTO

--- src/main/java/br/vagner/CadastroPessoas/Pessoas/PessoaDTO.java
+++ src/main/java/br/vagner/CadastroPessoas/Pessoas/PessoaDTO.java
@@ -1,4 +1,22 @@
 package br.vagner.CadastroPessoas.Pessoas;
 
+import br.vagner.CadastroPessoas.Atividades.Atividade;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//Classe para tirar a responsabilidade da entidade Pessoa, não acessando diretamente minha entidade
 public class PessoaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String statusSocial;
+    private Atividade atividades;
+
 }
--- src/main/java/br/vagner/CadastroPessoas/Pessoas/PessoaMapper.java
+++ src/main/java/br/vagner/CadastroPessoas/Pessoas/PessoaMapper.java
@@ -1,4 +1,33 @@
 package br.vagner.CadastroPessoas.Pessoas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PessoaMapper {
+
+    public Pessoa map(PessoaDTO pessoaDTO){
+        Pessoa pessoa = new Pessoa();
+        pessoa.setId(pessoaDTO.getId());
+        pessoa.setNome(pessoaDTO.getNome());
+        pessoa.setEmail(pessoaDTO.getEmail());
+        pessoa.setAtividades(pessoaDTO.getAtividades());
+        pessoa.setStatusSocial(pessoaDTO.getStatusSocial());
+        pessoa.setAtividades(pessoaDTO.getAtividades());
+
+        return pessoa;
+    }
+
+    public PessoaDTO map(Pessoa pessoa){
+            PessoaDTO pessoaDTO = new PessoaDTO();
+            pessoaDTO.setId(pessoa.getId());
+            pessoaDTO.setNome(pessoa.getNome());
+            pessoaDTO.setEmail(pessoa.getEmail());
+            pessoaDTO.setIdade(pessoa.getIdade());
+            pessoaDTO.setEmail(pessoa.getEmail());
+            pessoaDTO.setStatusSocial(pessoa.getStatusSocial());
+            pessoaDTO.setAtividades(pessoa.getAtividades());
+
+            return pessoaDTO;
+    }
+
 }
