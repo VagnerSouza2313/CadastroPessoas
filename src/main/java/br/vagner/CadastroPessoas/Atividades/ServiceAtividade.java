1b1d162de58fafc400b367933caaf46545a5f809
Vagner Souza <juninfps0@gmail.com>
2026-03-06 22:09:49 -0300

    feat: CRUD completo criado da regra de negócio

--- src/main/java/br/vagner/CadastroPessoas/Atividades/ServiceAtividade.java
+++ src/main/java/br/vagner/CadastroPessoas/Atividades/ServiceAtividade.java
@@ -1,19 +1,57 @@
 package br.vagner.CadastroPessoas.Atividades;
+import br.vagner.CadastroPessoas.Pessoas.PessoaMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceAtividade {
 
     RepositoryAtividade repositoryAtividades;
+    AtividadeMapper atividadeMapper = new AtividadeMapper();
 
     public ServiceAtividade(RepositoryAtividade repositoryAtividades) {
         this.repositoryAtividades = repositoryAtividades;
     }
 
-    public List<Atividade> ListarAtividades(){
-        return repositoryAtividades.findAll();
+    //Criar Atividade
+    public AtividadeDTO criarAtividade(AtividadeDTO atividadeDTO){
+        Atividade atividade = atividadeMapper.map(atividadeDTO);
+        repositoryAtividades.save(atividade);
+        return atividadeMapper.map(atividade);
+
     }
+
+    //Listar Atividades
+    public List<AtividadeDTO> ListarAtividades(){
+        List<Atividade> atividadeList = repositoryAtividades.findAll();
+        return atividadeList.stream().map(atividadeMapper::map).collect(Collectors.toList());
+    }
+
+    //Listar por id
+    public AtividadeDTO atividadeID(Long id){
+        Optional<Atividade> atividade = repositoryAtividades.findById(id);
+        return atividade.map(atividadeMapper::map).orElse(null);
+    }
+
+    //Atualizar atividade
+    public AtividadeDTO atividadeAtualizar(Long id, AtividadeDTO atividadeDTO){
+        Optional<Atividade> atividadeExistente = repositoryAtividades.findById(id);
+        if (atividadeExistente.isPresent()){
+            Atividade atividadeAtualizada = atividadeMapper.map(atividadeDTO);
+            atividadeAtualizada.setId(id);
+            Atividade atividadeSalvo = repositoryAtividades.save(atividadeAtualizada);
+            return atividadeMapper.map(atividadeSalvo);
+        }
+        return null;
+    }
+
+    //Deletar atividade
+    public void deletarAtividade(Long id){
+        repositoryAtividades.deleteById(id);
+    }
+
 
 }
